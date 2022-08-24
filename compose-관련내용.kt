77be4947f99ed4a459a43b4c3f208bdfb9ae98d9
kimdohyung5 <game5780@gmail.com>
2022-08-24 14:36:19 +0900

    Create compose-관련내용.kt

---
+++ compose-관련내용.kt
@@ -0,0 +1,17 @@
+
+
+1) animation관련 내용
+   아래와 같이 설정을 하면 값이 변경이 된다.
+
+    var animationPlayed by remember {
+        mutableStateOf(false)
+    }
+    val curPercent = animateFloatAsState(
+        targetValue = if(animationPlayed) {
+            statValue / statMaxValue.toFloat()
+        } else 0f,
+        animationSpec = tween(
+            animDuration,
+            animDelay
+        )
+    )
