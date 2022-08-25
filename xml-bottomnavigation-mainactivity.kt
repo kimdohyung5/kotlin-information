85dafbca4098080d541b2ff42f427741784d25e8
kimdohyung5 <game5780@gmail.com>
2022-08-25 16:01:02 +0900

    Update xml-bottomnavigation-mainactivity.kt

--- xml-bottomnavigation-mainactivity.kt
+++ xml-bottomnavigation-mainactivity.kt
@@ -26,3 +26,54 @@ class MainActivity : AppCompatActivity() {
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
+
+
+
+
+//////////////////////////////////////////////
+메인에서 사용되는 다른 사용법
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate( layoutInflater )}
+
+    private lateinit var mNavController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView( binding.root )
+
+        setBottomNav()
+
+        changeSelectedItemId( R.id.fragment_home )
+    }
+
+    private fun setBottomNav() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController( navController )
+
+        val appBarConfiguration = AppBarConfiguration( setOf( R.id.fragment_home, R.id.fragment_info, R.id.fragment_setting) )
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
+
+        mNavController = navController
+
+        mNavController.addOnDestinationChangedListener { _, destination, _ ->
+            if(destination.id == R.id.fragment_info || destination.id == R.id.fragment_info_child) {
+                binding.toolbar.visibility = View.GONE
+                binding.bottomNavigationView.visibility = View.GONE
+            } else {
+                binding.toolbar.visibility = View.VISIBLE
+                binding.bottomNavigationView.visibility = View.VISIBLE
+            }
+        }
+    }
+
+    fun changeSelectedItemId( id: Int) {
+        binding.bottomNavigationView.selectedItemId = id
+    }
+
+    companion object {
+        const val TAG = "MainActivity"
+    }
+}
+
