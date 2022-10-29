10478f17b9638e9620a93947ad6c91d2f63ac827
kimdohyung5 <game5780@gmail.com>
2022-10-29 18:21:44 +0900

    Update Resource.kt

--- Resource.kt
+++ Resource.kt
@@ -4,3 +4,25 @@ sealed class Resource<T>(val data:T? = null,  val message:String? = null) {
     class Loading<T>(data:T? = null): Resource<T>(data)
 }
 
+
+//////////////////////////////////////////////////////////////////////////
+// 이건 여분으로 관련된 부분이다.
+sealed class Result<out R> {
+
+    data class Success<out T>(val data: T) : Result<T>()
+    data class Error(val exception: Exception) : Result<Nothing>()
+
+    override fun toString(): String {
+        return when (this) {
+            is Success<*> -> "Success[data=$data]"
+            is Error -> "Error[exception=$exception]"
+        }
+    }
+}
+
+/**
+ * `true` if [Result] is of type [Success] & holds non-null [Success.data].
+ */
+val Result<*>.succeeded
+    get() = this is Success && data != null
+//////////////////////////////////////////////////////////////////////////
